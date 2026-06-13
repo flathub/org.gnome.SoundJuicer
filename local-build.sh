3e213215b88c8725426355a0163d8a752faa0e70
Sabri Ünal <yakushabb@gmail.com>
2026-06-13 21:39:26 +0300

    Fix manifest-invalid-json linter warning
    By converting it to yaml.
    
    > 'manifest-invalid-json' warning found in linter manifest check. Details: ['org.gnome.SoundJuicer.json: Expecting value (line 8, column 9)'] (will become an error after '31 December 2026 UTC': '201' days remaining).
    > Manifest(s) must be valid JSON per RFC 7159

--- local-build.sh
+++ local-build.sh
@@ -5,5 +5,5 @@ BRANCH=test
 mkdir _build
 mkdir _repo
 
-flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build org.gnome.SoundJuicer.json --repo=_repo
+flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build org.gnome.SoundJuicer.yaml --repo=_repo
 flatpak build-bundle _repo org.gnome.SoundJuicer.flatpak org.gnome.SoundJuicer $BRANCH
